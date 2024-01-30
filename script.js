b259993aab60066755122c3c7116d1460d9df211
SwamiNaidu5899 <swaminaidu874@gmail.com>
2024-01-30 19:42:09 +0530

    add

---
+++ script.js
@@ -0,0 +1,28 @@
+function topic1()
+{
+    document.getElementById("topic-2").style.display = 'none';
+    document.getElementById("topic-2").style.display = 'none';
+    document.getElementById("topic-1").style.display = 'inline';
+    document.getElementById("topic-4").style.display = 'none';
+}
+function topic2()
+{
+    document.getElementById("topic-1").style.display = 'none';
+    document.getElementById("topic-2").style.display = 'inline';
+    document.getElementById("topic-3").style.display = 'none';
+    document.getElementById("topic-4").style.display = 'none';
+}
+function topic3()
+{
+    document.getElementById("topic-1").style.display = 'none';
+    document.getElementById("topic-2").style.display = 'none';
+    document.getElementById("topic-3").style.display = 'inline';
+    document.getElementById("topic-4").style.display = 'none';
+}
+function topic4()
+{
+    document.getElementById("topic-1").style.display = 'none';
+    document.getElementById("topic-2").style.display = 'none';
+    document.getElementById("topic-3").style.display = 'none';
+    document.getElementById("topic-4").style.display = 'inline';
+}
